8ae112f4823b770ca7a18cf5fb47356746d489d3
hellsoon <pmaks2365@gmail.com>
2026-02-16 09:15:50 +0300

    feat: editor

--- app/components/features/note-editor/model/use-crepe.ts
+++ app/components/features/note-editor/model/use-crepe.ts
@@ -1,5 +1,7 @@
 import type { ModelRef } from 'vue'
 import { Crepe } from '@milkdown/crepe'
+import { listener, listenerCtx } from '@milkdown/kit/plugin/listener'
+import { replaceAll } from '@milkdown/kit/utils'
 import '@milkdown/crepe/theme/common/style.css'
 import '@milkdown/crepe/theme/nord-dark.css'
 
@@ -15,11 +17,28 @@ export function useCrepe(
 
     const instance = new Crepe({
       root: elRef.value,
-      defaultValue: model.value,
+      defaultValue: model.value || '',
     })
+
+    instance.editor
+      .config((ctx) => {
+        ctx.get(listenerCtx).markdownUpdated((_, markdown) => {
+          if (model.value !== markdown) {
+            model.value = markdown
+          }
+        })
+      })
+      .use(listener)
 
     await instance.create()
     crepe.value = instance
+  })
+
+  watch(model, (newVal) => {
+    const instance = crepe.value
+    if (instance && newVal !== undefined && newVal !== instance.getMarkdown()) {
+      instance.editor.action(replaceAll(newVal))
+    }
   })
 
   onBeforeUnmount(() => {
