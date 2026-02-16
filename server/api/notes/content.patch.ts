15fbc68b5e1bfaf6f4b0fcf8f0914109ce8cc2db
hell-soon <pmaks2365@gmail.com>
2026-02-16 12:34:37 +0300

    feat: time for commit

--- server/api/notes/content.patch.ts
+++ server/api/notes/content.patch.ts
@@ -4,13 +4,15 @@ import { getGitProvider } from '~~/server/services/git'
 export default defineEventHandler(async (event) => {
   const body = await readBody<UpdateFileOptions>(event)
   const git = await getGitProvider(event)
+  const date = new Date().toISOString().split('T')[0]
+  const time = new Date().toTimeString().split(' ')[0]
 
   try {
     const { newSha, commitSha } = await git.updateFile({
       path: body.path,
       content: body.content,
       sha: body.sha,
-      message: body.message || `Update ${body.path}`,
+      message: body.message || `Update ${body.path} -- ${date} ${time}`,
       author: body.author,
     })
 
